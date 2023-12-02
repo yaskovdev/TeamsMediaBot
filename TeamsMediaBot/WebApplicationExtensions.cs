ebd6834b99bb2d4b4f4c15930707d61257616863
Sergei Iaskov <sergeiiaskov@microsoft.com>
2023-12-02 02:49:23 +0200

    Instantiate ITeamsMediaBotService on startup to join the calls faster.

---
+++ TeamsMediaBot/WebApplicationExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace TeamsMediaBot;
+
+public static class WebApplicationExtensions
+{
+    public static void InstantiateService(this WebApplication app, Type serviceType)
+    {
+        using var serviceScope = app.Services.CreateScope();
+        serviceScope.ServiceProvider.GetRequiredService(serviceType);
+    }
+}
